b2c3a0783aa9415b46b1be7a1468084f4b35d7ef
lokcito <enciso.leo@gmail.com>
2022-09-30 10:00:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tensorflow-images
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-uwiv13)
---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import { Component, createSignal } from 'solid-js';
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import "@tensorflow/tfjs";
+
+const App: Component = () => {
+  const [result, setResult] = createSignal([]);
+  const [load, setLoad] = createSignal(false);
+
+  let randomString = () => {
+    var chars = '01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz'; 
+    var stringlength = 5; /* could be 6 or 7, but takes forever because there are lots of dead images */
+    var text = '';
+    for (var i = 0; i < stringlength; i++) {
+      var rnum = Math.floor(Math.random() * chars.length);
+      text += chars.substring(rnum,rnum+1);
+    }
+     
+    var source = 'https://i.imgur.com/' + text + '.jpg';
+    
+    var image = new Image();
+    const img = document.getElementById("img");
+    img.src = source;
+  }
+
+  return (
+    <>
+      <section class="container px-20 mx-auto">
+        <div class="flex flex-col text-center w-full mb-6 mt-6">
+          <h1 class="font-medium text-2xl">Reconcimiento de imagenes</h1>
+        </div>
+        <div class="flex">
+          <div class="w-1/2 flex flex-col">
+            <div class="mx-auto border-2 border-gray-200 p-10 rounded-lg">
+              <div class="pb-3 text-center">
+                <button class="bg-indigo-500 text-white rounded px-10"
+                  onClick={async () => {
+                    setLoad(!load());
+                    const img = document.getElementById("img");
+                    // Load the model.
+                    const model = await mobilenet.load();
+                    // Classify the image.
+                    const predictions = await model.classify(img);
+
+                    //console.log('Predictions: ');
+                    //console.log(predictions);
+                    setLoad(!load());
+                    setResult(predictions);
+                  }}
+                >
+                  Predecir
+                </button>
+              </div>
+              <img id="img"  crossorigin='anonymous' class="w-full" src="https://i.imgur.com/BDDnWw4.jpeg" />
+              <div class="pt-3 text-center">
+                <button class="bg-indigo-500 text-white rounded px-10"
+                  onClick={() => {
+                    randomString();
+                  }}
+                >
+                  Cambiar imagen
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="w-1/2 flex flex-col">
+            <div class="mx-auto w-full border-2 border-gray-200 p-10 rounded-lg flex-1">
+              {load()?<h2 class="text-center text-xl">Analizando</h2>:<>
+              <h2 class="text-center text-xl">Prediccion</h2>
+              <div>
+                { result().map(e => <li>{e.className}</li>) }
+              </div></>}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default App;
